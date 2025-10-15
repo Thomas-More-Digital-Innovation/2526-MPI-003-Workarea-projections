6f808f6944abc5cded441dc59683b1a4e6f77e7f
r0987051 <145450345+r0987051@users.noreply.github.com>
2025-10-15 11:36:35 +0200

    inputField component + deel van Add grid modal

--- code/src/components/Popup.tsx
+++ code/src/components/Popup.tsx
@@ -1,9 +1,11 @@
 "use client";
 
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
+
 import Button from "./Button";
 import { XMarkIcon } from "@heroicons/react/20/solid";
 import { CloudArrowUpIcon, ArrowUpOnSquareIcon, ArrowDownOnSquareIcon } from "@heroicons/react/24/outline";
+import InputField from "@/components/InputField";
 
 const Popup = ({ popupType }: { popupType: string }) => {
   const fileInputRef = useRef<HTMLInputElement>(null);
@@ -23,10 +25,15 @@ const Popup = ({ popupType }: { popupType: string }) => {
   const images = Array(3).fill("praline.jpeg");
 
   const title =
-    popupType === "imageUpload" ? "Foto Toevoegen" : 
-    popupType === "exportImport" ? "Importeer / Exporteer Presets" : 
-    popupType === "removeImage" ? "Foto Verwijderen": 
-    "Popup";
+    popupType === "imageUpload" ? "Foto Toevoegen" :
+      popupType === "exportImport" ? "Importeer / Exporteer Presets" :
+        popupType === "removeImage" ? "Foto Verwijderen" :
+          "Popup";
+
+
+  const [amount, setAmount] = useState("");
+  const [shape, setShape] = useState("");
+  const [size, setSize] = useState("");
 
   return (
     <div className="p-4 bg-[var(--color-popup)] rounded-2xl shadow relative max-w-[609px] w-[80%] md:w-[40%]">
@@ -55,7 +62,7 @@ const Popup = ({ popupType }: { popupType: string }) => {
               <p className="text-gray-400">PNG, JPG, GIF tot 5MB</p>
             </div>
 
-            <p className="my-3 text-xl text-[var(--color-text)]">Of kies uit eerdere foto's</p>
+            <p className="my-3 text-xl text-[var(--color-text)]">Of kies uit eerdere foto&apos;s</p>
             <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mb-6">
               {images.map((src, idx) => (
                 <img
@@ -70,30 +77,55 @@ const Popup = ({ popupType }: { popupType: string }) => {
         )}
 
         {popupType === "gridPreset" && (
-          <div>{/* Hier komt code wat getoond moet worden */}</div>
+          <div className="w-full flex justify-center">
+            <div className="my-2 mx-1 flex flex-row gap-6 max-w-5xl mx-auto">
+              <InputField
+                type="textField"
+                label="Aantal"
+                hint="Aantal vormen"
+                value={amount}
+                onChange={setAmount}
+              />
+
+              <InputField
+                type="shapeDropdown"
+                label="Vorm"
+                value={shape}
+                onChange={setShape}
+              />
+
+              <InputField
+                type="sizeDropdown"
+                label="Grootte"
+                value={size}
+                onChange={setSize}
+              />
+            </div>
+          </div>
+
         )}
 
         {popupType === "exportImport" && (
           <div className="w-full flex justify-center">
-            <div 
+            <div
               className="w-[95%] h-[25vw] m-2 flex justify-center items-center rounded-2xl shadow cursor-pointer transition-colors duration-200"
               style={{ backgroundColor: hoveredBox === 'export' ? 'var(--hover-white)' : 'var(--color-white)' }}
               onMouseEnter={() => setHoveredBox('export')}
               onMouseLeave={() => setHoveredBox(null)}
             >
               <div className="flex-col justify-center text-center">
-                <ArrowUpOnSquareIcon className="h-30 w-30 text-[var(--color-primary)]"/>
+                <ArrowUpOnSquareIcon className="h-30 w-30 text-[var(--color-primary)]" />
                 <p className="mt-4 text-4xl">Export</p>
               </div>
             </div>
-            <div 
+            <div
               className="w-[95%] h-[25vw] m-2 flex justify-center items-center rounded-2xl shadow cursor-pointer transition-colors duration-200"
               style={{ backgroundColor: hoveredBox === 'import' ? 'var(--hover-white)' : 'var(--color-white)' }}
               onMouseEnter={() => setHoveredBox('import')}
               onMouseLeave={() => setHoveredBox(null)}
             >
               <div className="flex-col justify-center text-center">
-                <ArrowDownOnSquareIcon className="h-30 w-30 text-[var(--color-primary)]"/>
+                <ArrowDownOnSquareIcon className="h-30 w-30 text-[var(--color-primary)]" />
                 <p className="mt-4 text-4xl">Import</p>
               </div>
             </div>
@@ -101,19 +133,19 @@ const Popup = ({ popupType }: { popupType: string }) => {
         )}
 
         {popupType === "removeImage" && (
-            <div className="flex-col">
-               <div className="flex justify-center">
-                 <img
-                  src="praline.jpeg"
-                  alt="doosje praline"
-                  className="w-70 h-70 object-cover rounded-2xl shadow"
-                    />
-                </div>
-            <p className="mt-8 mb-8 text-3xl">    
+          <div className="flex-col">
+            <div className="flex justify-center">
+              <img
+                src="praline.jpeg"
+                alt="doosje praline"
+                className="w-70 h-70 object-cover rounded-2xl shadow"
+              />
+            </div>
+            <p className="mt-8 mb-8 text-3xl">
               Ben je zeker dat je dit wilt verwijderen?
             </p>
 
-            </div>
+          </div>
         )}
 
         <div className="w-full mt-4">
